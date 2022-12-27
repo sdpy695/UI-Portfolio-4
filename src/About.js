93b8192fa3ea8992a38dbc676e9968c961fd13b3
sdpy695uw325fyuv <sdpy695uw325fyuv@gmail.com>
2022-12-27 18:55:23 -0500

    ugh

--- src/About.js
+++ src/About.js
@@ -5,40 +5,39 @@ import "./App.css";
 import { useState } from "react";
 
 import NavBar from './components/NavBar';
-import WorkGallery from "./components/WorkGallery";
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import IconButton from '@mui/material/IconButton';
-
 
 const About = () => {
   return(
-        <div className="mainApp"> 
+        <div>
+            <h6>Hi there!</h6>
+                                
+            <p>
+            My name is Chloe and I am a junior user interface designer based in Providence, RI. I have a passion for creating intuitive and appealing designs that enhance the user experience. My design style is influenced by accessibility, functionality, and a strong focus on user-centered design principles.
+            </p>
+
+            <p>
+            I'm currently in my third year of Computer Science at Brown University. I've gained valuable experience through internships at <a href="https://www.sasaki.com/">Sasaki</a> and the <a href="https://www.sustaininfrastructure.org/">Center for Sustainable Infrastructure</a>, and a term at the <a href="https://berlinartinstitute.com/">Berlin Art Institute</a>. I have a strong foundation in design principles and am proficient in design software such as Adobe Creative Suite and Figma.
+            </p>
+
+            <p>
+            In my spare time, you can find me playing ultimate frisbee, drawing, and tinkering with code. I am always looking for new opportunities to grow and improve as a designer, and I am excited to see where my career takes me next.
+            </p>
+            <p>
+            Thank you for taking the time to visit my portfolio. I am always looking for opportunities to grow and learn as a designer, and I am excited to bring my skills and creativity to new challenges.
+            </p>
+        </div>
+        // <div className="mainApp"> 
          
 
-            <div className="AppCenter">
+        //     <div className="AppCenter">
 
-                 <div style="margin: 30% 15% 10% 15%" id="about!">
+        //          <div style="margin: 30% 15% 10% 15%" id="about!">
                     
-                    <h6>Hi there!</h6>
-                        
-                        <p>
-                        My name is Chloe and I am a junior user interface designer based in Providence, RI. I have a passion for creating intuitive and appealing designs that enhance the user experience. My design style is influenced by accessibility, functionality, and a strong focus on user-centered design principles.
-                        </p>
-        
-                         <p>
-                        I'm currently in my third year of Computer Science at Brown University. I've gained valuable experience through internships at <a href="https://www.sasaki.com/">Sasaki</a> and the <a href="https://www.sustaininfrastructure.org/">Center for Sustainable Infrastructure</a>, and a term at the <a href="https://berlinartinstitute.com/">Berlin Art Institute</a>. I have a strong foundation in design principles and am proficient in design software such as Adobe Creative Suite and Figma.
-                        </p>
-        
-                        <p>
-                        In my spare time, you can find me playing ultimate frisbee, drawing, and tinkering with code. I am always looking for new opportunities to grow and improve as a designer, and I am excited to see where my career takes me next.
-                        </p>
-                        <p>
-                        Thank you for taking the time to visit my portfolio. I am always looking for opportunities to grow and learn as a designer, and I am excited to bring my skills and creativity to new challenges.
-                         </p>
-                </div> 
-             </div>
+                   
+        //         </div> 
+        //      </div>
           
-        </div>
+        // </div>
   )
 
 
