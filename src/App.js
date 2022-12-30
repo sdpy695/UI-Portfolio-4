d5a8f85fc8bcd317bff29d118da49c24986329be
sdpy695uw325fyuv <sdpy695uw325fyuv@gmail.com>
2022-12-30 14:59:47 -0500

    still no about

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
            <Route path="/" element={Home}/>  
           <Route path="UI-Portfolio-4" element={Home}/>  
           <Route path="UI-Portfolio-4/about" element={About}/>  
-        
+          <Route path="about" element={About}/>  
 
         </Routes>
       </BrowserRouter>
